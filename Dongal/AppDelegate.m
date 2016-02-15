ce9b8cf16b92ae4a67c0c28d39512c5b6125e613
HanSJin <kksd9900@naver.com>
2016-02-15 18:04:16 +0900

    update

--- Dongal/AppDelegate.m
+++ Dongal/AppDelegate.m
@@ -8,8 +8,11 @@
 
 #import "AppDelegate.h"
 #import "AppDelegateMethods.h"
+#import "KeychainHelper.h"
+#import "SingletonData.h"
 
 #import "Constants.h"
+#import "ConnectionFactory.h"
 
 @interface AppDelegate ()
 
@@ -20,6 +23,20 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    // APNS에 디바이스를 등록한다.
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0) {
+        [[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge) categories:nil]];
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+    }
+    else {
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:
+         (UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert)];
+    }
+    application.applicationIconBadgeNumber = 0;
+    
+    
+    
     
     self.tabBarController = [[UITabBarController alloc] init];
     self.tabBarController.viewControllers = [AppDelegateMethods initTabbarViewControllerArray];
@@ -38,6 +55,34 @@
     
     return YES;
 }
+
+
+// 성공시
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSLog(@"[App Delegate Call] : didRegisterForRemoteNotificationsWithDeviceToken");
+    // DATA -> trimSTR
+    const unsigned *tokenBytes = [deviceToken bytes];
+    NSString *hexToken = [NSString stringWithFormat:@"%08x%08x%08x%08x%08x%08x%08x%08x",
+                          ntohl(tokenBytes[0]), ntohl(tokenBytes[1]), ntohl(tokenBytes[2]),
+                          ntohl(tokenBytes[3]), ntohl(tokenBytes[4]), ntohl(tokenBytes[5]),
+                          ntohl(tokenBytes[6]), ntohl(tokenBytes[7])];
+    NSLog(@"push token : %@", hexToken);
+    
+    // trimSTR -> DATA
+    NSData* hexTokenData = [hexToken dataUsingEncoding:NSUTF8StringEncoding];
+    
+    // 푸시 토큰 저장 - 키체인
+    [KeychainHelper saveWithKey:@"push_token_dongal" andData:hexTokenData];
+    
+    
+    
+    SingletonData *sharedMan = [SingletonData sharedManager];
+    NSString *postParams = [NSString stringWithFormat:@"uuid=%@&token=%@", sharedMan.UUID, hexToken];
+    NSData *myData = [ConnectionFactory connType:@"POST" connAPI:CONNECT_POST_PUSH connParam:postParams];
+    NSMutableDictionary *result = [NSJSONSerialization JSONObjectWithData:myData options:NSJSONReadingMutableContainers error:nil];
+}
+
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
